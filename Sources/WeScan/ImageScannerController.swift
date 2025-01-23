b9875cb94a93b026e5e45923e7f7c233de4d4e21
AvniTala <Avni.aspiration@gmail.com>
2025-01-23 10:51:42 +0530

    change in xcode 15 support

--- Sources/WeScan/ImageScannerController.swift
+++ Sources/WeScan/ImageScannerController.swift
@@ -177,38 +177,79 @@ public struct ImageScannerScan {
 /// Data structure containing information about a scanning session.
 /// Includes the original scan, cropped scan, detected rectangle, and whether the user selected the enhanced scan.
 /// May also include an enhanced scan if no errors were encountered.
-public struct ImageScannerResults {
+//public struct ImageScannerResults {
+//
+//    /// The original scan taken by the user, prior to the cropping applied by WeScan.
+//    public var originalScan: ImageScannerScan
+//
+//    /// The deskewed and cropped scan using the detected rectangle, without any filters.
+//    public var croppedScan: ImageScannerScan
+//
+//    /// The enhanced scan, passed through an Adaptive Thresholding function.
+//    /// This image will always be grayscale and may not always be available.
+//    public var enhancedScan: ImageScannerScan?
+//
+//    /// Whether the user selected the enhanced scan or not.
+//    /// The `enhancedScan` may still be available even if it has not been selected by the user.
+//    public var doesUserPreferEnhancedScan: Bool
+//
+//    /// The detected rectangle which was used to generate the `scannedImage`.
+//    public var detectedRectangle: Quadrilateral
+//
+//    init(
+//        detectedRectangle: Quadrilateral,
+//        originalScan: ImageScannerScan,
+//        croppedScan: ImageScannerScan,
+//        enhancedScan: ImageScannerScan?,
+//        doesUserPreferEnhancedScan: Bool = false
+//    ) {
+//        self.detectedRectangle = detectedRectangle
+//
+//        self.originalScan = originalScan
+//        self.croppedScan = croppedScan
+//        self.enhancedScan = enhancedScan
+//
+//        self.doesUserPreferEnhancedScan = doesUserPreferEnhancedScan
+//    }
+//}
 
+public struct ImageScannerResults {
+    
     /// The original scan taken by the user, prior to the cropping applied by WeScan.
     public var originalScan: ImageScannerScan
-
+    
     /// The deskewed and cropped scan using the detected rectangle, without any filters.
     public var croppedScan: ImageScannerScan
-
-    /// The enhanced scan, passed through an Adaptive Thresholding function.
-    /// This image will always be grayscale and may not always be available.
+    
+    /// The enhanced scan, passed through an Adaptive Thresholding function. This image will always be grayscale and may not always be available.
     public var enhancedScan: ImageScannerScan?
-
+    
     /// Whether the user selected the enhanced scan or not.
     /// The `enhancedScan` may still be available even if it has not been selected by the user.
     public var doesUserPreferEnhancedScan: Bool
-
+    
     /// The detected rectangle which was used to generate the `scannedImage`.
     public var detectedRectangle: Quadrilateral
-
-    init(
-        detectedRectangle: Quadrilateral,
-        originalScan: ImageScannerScan,
-        croppedScan: ImageScannerScan,
-        enhancedScan: ImageScannerScan?,
-        doesUserPreferEnhancedScan: Bool = false
-    ) {
+//
+//    @available(*, unavailable, renamed: "originalScan")
+//    public var originalImage: UIImage?
+//
+//    @available(*, unavailable, renamed: "croppedScan")
+//    public var scannedImage: UIImage?
+//
+//    @available(*, unavailable, renamed: "enhancedScan")
+//    public var enhancedImage: UIImage?
+//
+//    @available(*, unavailable, renamed: "doesUserPreferEnhancedScan")
+//    public var doesUserPreferEnhancedImage: Bool = false
+    
+    init(detectedRectangle: Quadrilateral, originalScan: ImageScannerScan, croppedScan: ImageScannerScan, enhancedScan: ImageScannerScan?, doesUserPreferEnhancedScan: Bool = false) {
         self.detectedRectangle = detectedRectangle
-
+        
         self.originalScan = originalScan
         self.croppedScan = croppedScan
         self.enhancedScan = enhancedScan
-
+        
         self.doesUserPreferEnhancedScan = doesUserPreferEnhancedScan
     }
 }
